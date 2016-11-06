bd9f1ae791519f23cd75e159a5cc49bf354fca6a
Eedrah <eedrah@users.noreply.github.com>
2016-11-05 22:42:39 -0600

    set up styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
+const styles = {
+  page: {
+    'background-color': '#804080'
+  },
+  heading: {
+    'color': '#A02020'
+  },
+  subheading: {
+    'color': '#2020A0'
+  }
+}
+
 class App extends React.Component {
   render () {
     return (
-      <div>
-        <h1>Jesse, are you still alive?!!</h1>
-        <p>Yes, yes I am</p>
+      <div style={styles.page}>
+        <h1 style={styles.heading}>Jesse, are you still alive?!!</h1>
+        <p style={styles.subheading}>Yes, yes I am</p>
       </div>
     )
   }
