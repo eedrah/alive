91e41c030e95ba548cb11ed26ade9e42a4966a38
Eedrah <eedrah@users.noreply.github.com>
2016-11-10 03:25:47 -0600

    Show status from firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import db from './db'
+
 const styles = {
   page: {
-    'background-color': '#804080'
+    'backgroundColor': '#804080'
   },
   heading: {
     'color': '#A02020'
@@ -13,11 +15,22 @@ const styles = {
 }
 
 class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { lastCheckIn: null }
+  }
+  componentWillMount () {
+    db.ref('lastCheckIn').on('value', (data) => {
+      this.setState({
+        lastCheckIn: data.val()
+      })
+    })
+  }
   render () {
     return (
       <div style={styles.page}>
         <h1 style={styles.heading}>Jesse, are you still alive?!!</h1>
-        <p style={styles.subheading}>Yes, yes I am</p>
+        <p style={styles.subheading}>{'Yes, yes I am, as at ' + this.state.lastCheckIn}</p>
       </div>
     )
   }
