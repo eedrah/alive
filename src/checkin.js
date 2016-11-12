081d20c436668c5e2d05009c329cb01d3e09a33b
Eedrah <eedrah@users.noreply.github.com>
2016-11-12 07:24:50 -0600

    redirect to https

--- src/checkin.js
+++ src/checkin.js
@@ -3,6 +3,11 @@ import { render } from 'react-dom'
 
 import CheckinApp from './CheckinApp'
 
-// const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production'
+if (isProd) {
+  if ((window.location.host === 'eedrah.com') && (window.location.protocol !== 'https:')) {
+    window.location.protocol = 'https'
+  }
+}
 
 render((<CheckinApp />), document.querySelector('#app'))
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import { render } from 'react-dom'
 
 import App from './App'
 
-// const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production'
+if (isProd) {
+  if ((window.location.host === 'eedrah.com') && (window.location.protocol !== 'https:')) {
+    window.location.protocol = 'https'
+  }
+}
 
 render((<App />), document.querySelector('#app'))
